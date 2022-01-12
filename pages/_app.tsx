c332e1d9e650d3be1c7ca52f3dd8249dc5e20a52
David Michael II <knowdavidmichael@gmail.com>
2022-01-12 01:37:20 +0100

    feat: add apollo config

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,22 +1,35 @@
 import React from 'react';
-import { AppProps } from 'next/app';
+import App, { AppProps } from 'next/app';
 import { Provider as StyletronProvider } from 'styletron-react';
 import { BaseProvider } from 'baseui';
+import { AppContext } from 'next/dist/pages/_app';
+import { ApolloProvider } from '@apollo/client';
 import TeekTheme from '../theme';
 import styletron from '../styletron';
+import { useApollo } from '../lib/apollo';
 
 import 'normalize.css';
 import '../styles/globals.css';
 
 function TeekApp(props: AppProps) {
   const { Component, pageProps } = props;
+  const { initialApolloState } = pageProps;
+  const apolloClient = useApollo(initialApolloState);
+
   return (
-    <StyletronProvider value={styletron}>
-      <BaseProvider theme={TeekTheme}>
-        <Component {...pageProps} />
-      </BaseProvider>
-    </StyletronProvider>
+    <ApolloProvider client={apolloClient}>
+      <StyletronProvider value={styletron}>
+        <BaseProvider theme={TeekTheme}>
+          <Component {...pageProps} />
+        </BaseProvider>
+      </StyletronProvider>
+    </ApolloProvider>
   );
 }
 
 export default TeekApp;
+
+TeekApp.getInitialProps = async (context: AppContext) => {
+  const appProps = await App.getInitialProps(context);
+  return { ...appProps };
+};
