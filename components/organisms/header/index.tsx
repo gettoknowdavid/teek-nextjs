5e760e30521e1520a7ba490f50701cd1f9a07049
David Michael II <knowdavidmichael@gmail.com>
2022-01-15 14:00:41 +0100

    refactor: header component

--- components/organisms/header/index.tsx
+++ components/organisms/header/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Block } from 'baseui/block';
-import { useStyletron } from 'baseui';
 import { Axis } from '../../../enums';
 import { NavItem } from '../../atoms/nav-item';
 import { NavList } from '../../molecules/nav-list';
@@ -8,16 +7,12 @@ import { Logo } from '../../atoms/logo';
 import { SignUpButton } from '../../atoms/sign-up-button';
 
 export function Header() {
-  const [css, theme] = useStyletron();
-
   return (
     <Block
-      height="60px"
-      paddingRight={['10px', '10px', '20px', '100px']}
-      paddingLeft={['10px', '10px', '20px', '100px']}
       overrides={{
         Block: {
           style: (props) => ({
+            height: '60px',
             position: 'sticky',
             top: '0px',
             backgroundColor: '#1D1E26',
@@ -28,24 +23,26 @@ export function Header() {
             color: props.$theme.colors.mono100,
             transitionProperty: 'all',
             transitionDuration: props.$theme.animation.timing700,
+            [props.$theme.mediaQuery.small]: { paddingRight: '10px', paddingLeft: '10px' },
+            [props.$theme.mediaQuery.medium]: { paddingRight: '20px', paddingLeft: '20px' },
+            [props.$theme.mediaQuery.large]: { paddingRight: '100px', paddingLeft: '100px' },
           }),
         },
       }}
     >
+
       <Logo />
 
-      <Block className={css({
-        width: '100%',
-        justifyContent: 'space-between',
-        alignItems: 'center',
-        [theme.mediaQuery.small]: { display: 'none' },
-        [theme.mediaQuery.medium]: { display: 'none' },
-        [theme.mediaQuery.large]: { display: 'flex' },
-      })}
+      <Block
+        width="100%"
+        display={['none', 'none', 'none', 'flex']}
+        justifyContent="space-between"
+        alignItems="center"
       >
         <Block display={['none', 'none', 'none', 'block']}>
           <NavList axis={Axis.HORIZONTAL} />
         </Block>
+
         <Block display="flex" alignItems="center" maxWidth="160px" width="100%" height="60px">
           <NavItem axis={Axis.HORIZONTAL} title="Login" onClick={() => null} />
           <SignUpButton onClick={() => null} />
