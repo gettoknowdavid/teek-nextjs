8dd76b24aea5114016e33acd6f41c1c79a56423e
David Michael II <knowdavidmichael@gmail.com>
2022-01-15 13:55:50 +0100

    refactor: sign up button

---
+++ components/atoms/sign-up-button/index.tsx
@@ -0,0 +1,52 @@
+import { Button } from 'baseui/button';
+import React from 'react';
+
+export function SignUpButton(props: { onClick: () => unknown }) {
+  return (
+    <Button
+      {...props}
+      overrides={{
+        BaseButton: {
+          style: ({ $theme }) => ({
+            fontSize: '14px',
+            width: '100%',
+            paddingTop: '6px',
+            paddingRight: '12px',
+            paddingBottom: '6px',
+            paddingLeft: '12px',
+            backgroundColor: 'transparent',
+            color: $theme.colors.mono100,
+            borderTopRightRadius: '8px',
+            borderBottomRightRadius: '8px',
+            borderBottomLeftRadius: '8px',
+            borderTopLeftRadius: '8px',
+            borderTopWidth: '1px',
+            borderTopStyle: 'solid',
+            borderTopColor: $theme.colors.mono300,
+            borderRightWidth: '1px',
+            borderRightStyle: 'solid',
+            borderRightColor: $theme.colors.mono300,
+            borderBottomWidth: '1px',
+            borderBottomStyle: 'solid',
+            borderBottomColor: $theme.colors.mono300,
+            borderLeftWidth: '1px',
+            borderLeftStyle: 'solid',
+            borderLeftColor: $theme.colors.mono300,
+            transitionProperty: 'all',
+            transitionDuration: $theme.animation.timing700,
+            ':hover': {
+              background: $theme.gradients.main,
+              color: 'white',
+              borderTopColor: 'transparent',
+              borderRightColor: 'transparent',
+              borderBottomColor: 'transparent',
+              borderLeftColor: 'transparent',
+            },
+          }),
+        },
+      }}
+    >
+      Sign up
+    </Button>
+  );
+}
--- components/organisms/header/index.tsx
+++ components/organisms/header/index.tsx
@@ -1,61 +1,11 @@
 import React from 'react';
 import { Block } from 'baseui/block';
 import { useStyletron } from 'baseui';
-import { Button } from 'baseui/button';
 import { Axis } from '../../../enums';
 import { NavItem } from '../../atoms/nav-item';
 import { NavList } from '../../molecules/nav-list';
 import { Logo } from '../../atoms/logo';
-
-function SignUpButton(props: { onClick: () => unknown }) {
-  return (
-    <Button
-      {...props}
-      overrides={{
-        BaseButton: {
-          style: ({ $theme }) => ({
-            fontSize: '14px',
-            width: '100%',
-            paddingTop: '6px',
-            paddingRight: '12px',
-            paddingBottom: '6px',
-            paddingLeft: '12px',
-            backgroundColor: 'transparent',
-            color: $theme.colors.mono100,
-            borderTopRightRadius: '8px',
-            borderBottomRightRadius: '8px',
-            borderBottomLeftRadius: '8px',
-            borderTopLeftRadius: '8px',
-            borderTopWidth: '1px',
-            borderTopStyle: 'solid',
-            borderTopColor: $theme.colors.mono300,
-            borderRightWidth: '1px',
-            borderRightStyle: 'solid',
-            borderRightColor: $theme.colors.mono300,
-            borderBottomWidth: '1px',
-            borderBottomStyle: 'solid',
-            borderBottomColor: $theme.colors.mono300,
-            borderLeftWidth: '1px',
-            borderLeftStyle: 'solid',
-            borderLeftColor: $theme.colors.mono300,
-            transitionProperty: 'all',
-            transitionDuration: $theme.animation.timing700,
-            ':hover': {
-              background: $theme.gradients.main,
-              color: 'white',
-              borderTopColor: 'transparent',
-              borderRightColor: 'transparent',
-              borderBottomColor: 'transparent',
-              borderLeftColor: 'transparent',
-            },
-          }),
-        },
-      }}
-    >
-      Sign up
-    </Button>
-  );
-}
+import { SignUpButton } from '../../atoms/sign-up-button';
 
 export function Header() {
   const [css, theme] = useStyletron();
